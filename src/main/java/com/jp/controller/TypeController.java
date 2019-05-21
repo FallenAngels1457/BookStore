a07c1630a13d6a0bf1c57b39a3a725eeae4c023c
Fallen Angels <1910817468@qq.com>
2019-05-21 19:18:44 +0800

    书籍类型的添加

--- src/main/java/com/jp/controller/TypeController.java
+++ src/main/java/com/jp/controller/TypeController.java
@@ -1,5 +1,35 @@
 package com.jp.controller;
 
-public class TypeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jp.po.Type;
+import com.jp.service.TypeService;
+
+@Controller
+@RequestMapping("/type")
+public class TypeController {
+	//添加类型
+	@Autowired
+	@Qualifier("typeService")
+	private TypeService typeService;
+	 @RequestMapping("/addType")
+	 public String addType(Model model) throws Exception{
+		System.out.println("sama添加图书类型信息请求");
+		return "back/book/book_type_add"; 
+	 }
+	 //添加类型提交
+	 @RequestMapping("/addTypeSubmit")
+    public String addTypeSubmit(Type type, Model model) throws Exception{
+		System.out.println("sama添加图书类型信息提交:"+type.getName());
+		typeService.addType(type);
+		return "forward:/type/toTypeList"; 
+	 } 	 
+	 
+	 
+	 
+	 
 }
